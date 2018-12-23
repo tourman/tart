538a230791e98d4a7a998fe8d5c2924bfff83149
tourman <8002615@gmail.com>
2018-12-23 15:33:30 +0200

    Fix for eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom';
 import app from 'containers/app-element';
 import dispatcher from 'dispatcher';
-import css from '../css/style.css';
+import '../css/style.css';
 
 const appEl = window.document.getElementById('app');
 ReactDOM.render(app, appEl);
 
-/* global MODE */
+/* eslint-disable */
 if (MODE === 'development') {
   window._dispatcher = dispatcher;
 }
+/* eslint-emable */
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
           'style-loader',
           'css-loader',
         ],
-      }
+      },
     ],
   },
   mode: 'development',
