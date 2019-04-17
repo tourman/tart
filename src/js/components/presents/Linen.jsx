18ccceee0f170ce7f9d1fabfcb2dc5ea85575434
tourman <8002615@gmail.com>
2019-04-17 17:13:22 +0300

    useRef()

--- src/js/components/presents/Linen.jsx
+++ src/js/components/presents/Linen.jsx
@@ -1,43 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Figure from './Linen/Figure';
 import dispatcher from '../../dispatcher';
-
-let ref;
 
 const typeMap = new Map()
   .set(true,  'negative')
   .set(false, 'positive')
 ;
 
-const mouse = name => e => dispatcher.dispatch({
-  name,
-  payload: {
-    x: e.clientX - ref.getBoundingClientRect().left,
-    y: e.clientY - ref.getBoundingClientRect().top,
-    type: typeMap.get(e.ctrlKey || e.shiftKey || e.button === 2),
-  },
-});
+const handleMouse = (el, name) => e => {
+  const rect = el.current.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  const typeCondition = e.ctrlKey || e.shiftKey || e.button === 2;
+  const type = typeMap.get(typeCondition);
+  dispatcher.dispatch({
+    name,
+    payload: {
+      x,
+      y,
+      type,
+    },
+  });
+};
 
-const Linen = props => (
-  <div
-    className="linen app__element"
-    style={{
-      width: `${props.size}px`,
-      height: `${props.size}px`,
-    }}
-    ref={el => ref = el}
-    onMouseDown={mouse('figure.add')}
-    onMouseMove={mouse('figure.last.resize')}
-    onMouseUp={mouse('figure.last.update')}
-  >
-    {props.figures.map((figure, index) => (
-      <Figure
-        {...figure}
-        index={index}
-        key={index}
-      />
-    ))}
-  </div>
-);
+const Linen = props => {
+  const el = useRef(null);
+  const mouse = handleMouse.bind(null, el);
+
+  return (
+    <div
+      className="linen app__element"
+      style={{
+        width: props.size,
+        height: props.size,
+      }}
+      ref={el}
+      onMouseDown={mouse('figure.add')}
+      onMouseMove={mouse('figure.last.resize')}
+      onMouseUp={mouse('figure.last.update')}
+    >
+      {props.figures.map((figure, index) => (
+        <Figure
+          {...figure}
+          index={index}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Linen;
