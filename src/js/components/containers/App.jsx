359d13092612e85640adffc8ffffcc42e9ce79e8
tourman <8002615@gmail.com>
2019-04-18 15:05:58 +0300

    no flux: props actions instead of action name

--- src/js/components/containers/App.jsx
+++ src/js/components/containers/App.jsx
@@ -5,8 +5,20 @@ import Linen from '../presents/Linen';
 import Info from '../presents/Info';
 import Store from '../../stores/linen';
 import dispatcher from '../../dispatcher';
+import { mapValues } from 'lodash';
 
 const store = new Store(dispatcher);
+
+const actionToTypeMap = {
+  onFigureAdd:        'figure.add',
+  onFigureLastResize: 'figure.last.resize',
+  onFigureLastUpdate: 'figure.last.update',
+};
+
+const actions = mapValues(actionToTypeMap, name => payload => dispatcher.dispatch({
+  name,
+  payload,
+}))
 
 class AppContent extends Component {
   static getStores() {
@@ -25,7 +37,7 @@ class AppContent extends Component {
     return (
       <React.StrictMode>
         <AppWrapper>
-          <Linen {...this.state} />
+          <Linen {...this.state} {...actions} />
           <Info {...this.state} />
         </AppWrapper>
       </React.StrictMode>
--- src/js/components/presents/Linen.jsx
+++ src/js/components/presents/Linen.jsx
@@ -1,32 +1,27 @@
 import React, { useRef } from 'react';
 import Figure from './Linen/Figure';
-import dispatcher from '../../dispatcher';
 
 const typeMap = new Map()
   .set(true,  'negative')
   .set(false, 'positive')
 ;
 
-const handleMouse = (el, name) => e => {
+const handleMouse = (el, action) => e => {
   const rect = el.current.getBoundingClientRect();
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
   const typeCondition = e.ctrlKey || e.shiftKey || e.button === 2;
   const type = typeMap.get(typeCondition);
-  dispatcher.dispatch({
-    name,
-    payload: {
-      x,
-      y,
-      type,
-    },
+  action({
+    x,
+    y,
+    type,
   });
 };
 
 const Linen = props => {
   const el = useRef(null);
-  const mouse = handleMouse.bind(null, el);
-
+  const onMouse = handleMouse.bind(null, el);
   return (
     <div
       className="linen app__element"
@@ -35,9 +30,9 @@ const Linen = props => {
         height: props.size,
       }}
       ref={el}
-      onMouseDown={mouse('figure.add')}
-      onMouseMove={mouse('figure.last.resize')}
-      onMouseUp={mouse('figure.last.update')}
+      onMouseDown={onMouse(props.onFigureAdd)}
+      onMouseMove={onMouse(props.onFigureLastResize)}
+      onMouseUp={onMouse(props.onFigureLastUpdate)}
     >
       {props.figures.map((figure, index) => (
         <Figure
