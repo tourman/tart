a2df4f229add5190b290bdeb826f35f55249d95a
tourman <8002615@gmail.com>
2019-04-26 13:42:57 +0300

    refactoring: HOC with throttle

--- src/js/components/presents/Info.jsx
+++ src/js/components/presents/Info.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Total from './Info/Total';
 import ItemSet from './Info/ItemSet';
+import { throttle } from 'lodash';
 
 const Info = props => (
   <>
@@ -9,4 +10,17 @@ const Info = props => (
   </>
 );
 
-export default Info;
+const throttleInfo = Info => {
+  const infoMap = new Map()
+    .set(true, throttle(Info, 200))
+    .set(false, Info)
+    .set(void 0, Info)
+  ;
+  const ThrottledInfo = props => {
+    const Info = infoMap.get(props.openForResizing);
+    return (<Info {...props} />);
+  };
+  return ThrottledInfo;
+};
+
+export default throttleInfo(Info);
