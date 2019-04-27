91261a94c8b400d45438fde6d3b350e0d33d3322
tourman <8002615@gmail.com>
2019-04-27 14:17:41 +0300

    Updates

---
+++ precache-manifest.34e9b9c86e249b68f60a70ac582ca313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef478a0d5d7b252464f3",
+    "url": "/static/css/main.dc278749.chunk.css"
+  },
+  {
+    "revision": "ef478a0d5d7b252464f3",
+    "url": "/static/js/main.8e006f9e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "06671cb618874d5e8e52",
+    "url": "/static/js/2.af26b250.chunk.js"
+  },
+  {
+    "revision": "68275f7f061037e9ddd49333c27cb3e2",
+    "url": "/index.html"
+  }
+];
