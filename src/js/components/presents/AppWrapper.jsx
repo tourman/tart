b26bf45b4cf5b81da6d931fc84d48f8c8efe3623
tourman <8002615@gmail.com>
2019-04-27 19:37:52 +0300

    throttle delay from styles

--- src/js/components/presents/AppWrapper.jsx
+++ src/js/components/presents/AppWrapper.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../../../css/style.css';
 
 const silentFocus = action => {
   const { scrollX, scrollY } = window;
@@ -6,9 +7,13 @@ const silentFocus = action => {
   window.scrollTo(scrollX, scrollY);
 };
 
-const throttleDelay = 200;
-
 const AppWrapper = props => {
+  const [ throttleDelay, setThrottleDelay ] = useState(0);
+  useEffect(() => {
+    const cssRule = [].find.call(document.styleSheets[0].cssRules, cssRule => cssRule.selectorText.includes('.figure_throttle'));
+    const transitionDuration = parseInt(cssRule.style.transitionDuration);
+    setThrottleDelay(transitionDuration);
+  });
   const children = React.Children.map(
     props.children,
     child => React.cloneElement(child, {
